0d7b2bad05b7c362c98a7b6a937bb3a6b099cfdd
bleakley <brianbleakley@gmail.com>
2017-06-28 20:40:30 -0600

    restored trailing spaces

--- Brogue Source/BrogueCode/Rogue.h
+++ Brogue Source/BrogueCode/Rogue.h
@@ -31,7 +31,7 @@
 #define USE_UNICODE
 
 // version string -- no more than 16 bytes:
-#define BROGUE_VERSION_STRING "1.7.4.0"
+#define BROGUE_VERSION_STRING "1.7.4.1"
 
 // debug macros -- define DEBUGGING as 1 to enable wizard mode.
 
